1cacbec9ac1eb70a24b74e1ac9b6c0b99a1d9a18
ljt0815 <ljt0815@naver.com>
2023-06-13 14:12:29 +0900

    프로젝트 시작

---
+++ src/main/java/shop/jitlee/parchment/ParchmentApplication.java
@@ -0,0 +1,13 @@
+package shop.jitlee.parchment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParchmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParchmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/jitlee/parchment/ParchmentApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.jitlee.parchment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParchmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
